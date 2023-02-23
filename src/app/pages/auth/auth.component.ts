a2006c0a031bccf4628802b3673bc55f83eeb2d9
Geek DJENIKA <djenikaa@gmail.com>
2023-02-23 19:57:08 +0000

    Connection and sign up binded

--- src/app/pages/auth/auth.component.ts
+++ src/app/pages/auth/auth.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth/auth.service';
 import Swal from 'sweetalert2';
 
@@ -16,8 +17,10 @@ export class AuthComponent implements OnInit {
   npassword: any;
   ncpassword: any;
   nemail: any;
+
+  result : string = localStorage.getItem('signup')!
   
-  constructor(private authservice : AuthService) { }
+  constructor(private authservice : AuthService, private router: Router) { }
 
   ngOnInit(): void {
     const signUpButton = document.getElementById('signUp') as HTMLButtonElement;
@@ -37,8 +40,15 @@ export class AuthComponent implements OnInit {
   }
 
   signup(username : string, email:string, password1 : string, password2 : string) {
+    const container = document.getElementById('container') as HTMLElement;
+
     if(password1 == password2) {
       this.authservice.signUp(username, email,password1);
+      console.log(this.result)
+      if(this.result = 'true') {
+        container.classList.remove("right-panel-active");
+      }
+      
     } else {
       Swal.fire('Erreur mot de passe', 'Les mots de passe ne correspondent pas !', 'error')
     }
--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { tap } from 'rxjs';
+import Swal from 'sweetalert2';
 import { ConnexionService } from './connexion.service';
 import { StorageService } from './storage.service';
 
@@ -10,24 +12,37 @@ export class AuthService {
 
   constructor(
     private connexionservice : ConnexionService,
-    private store : StorageService) { }
+    private store : StorageService,
+    private router: Router) { }
 
   login(username: string, password: string) {
-    return this.connexionservice.signin(username, password).pipe(
-      tap((response: any) => {
-        this.store.enregistrer(response);
-        // this.store.connexionReussi
-      })
-    );
+    return this.connexionservice.signin(username, password).subscribe({
+      next: data => {
+        this.store.enregistrer(data);
+        console.log(data)
+        Swal.fire('Connecté !', 'Vous êtes connecté avec succès !', 'success')
+        this.router.navigate(['/dashboard/accueil'])
+      },
+      error: e => {
+        Swal.fire('Erreur', 'Nom d\'utilisateur ou mot de passe incorrecte !', 'error')
+      }
+    })
   }
 
   signUp(username: string, email: string, password: string) {
-    return this.connexionservice.signup(username, email, password).pipe(
-      tap((response: any) => {
-        //this.store.enregistrer(response);
-        // this.store.connexionReussi
-      })
-    );
+    console.log('trtman ...')
+    return this.connexionservice.signup(username, email, password).subscribe({
+      next: data => {
+        console.log(data)
+        Swal.fire('Compte créé !', 'Votre compte est créé avec succès !', 'success')
+        localStorage.setItem('signup','true')
+      },
+      error: e => {
+        console.log(e.error.message)
+        Swal.fire('Erreur', e.error.message, 'error')
+        localStorage.setItem('signup','false')
+      }
+    })
   }
 
 }
